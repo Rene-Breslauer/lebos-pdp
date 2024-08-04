74fc7e88e83f0b0df760b17cf12784d347bbbeaa
Rene Breslauer <121046070+Rene-Breslauer@users.noreply.github.com>
2024-08-04 09:53:48 -0400

    feat(pdp): tabs

--- src/components/store-availability/storeAvailability.ts
+++ src/components/store-availability/storeAvailability.ts
@@ -1,30 +1,45 @@
 import Swiper from 'swiper/bundle'
 export default (Alpine: any) => {
-  Alpine.data(
-    'storeAvailability', (defaultVariantId: number) => ({
-      locationModal: false,
-      pickupLocations: [],
-      defaultVariantId,
-      popupLocation: null as object | null,
+  Alpine.data('storeAvailability', (defaultVariantId: number) => ({
+    locationModal: false,
+    pickupLocations: [],
+    defaultVariantId,
+    popupLocation: null as object | null,
 
-      init() {
-          window.addEventListener('option-changed', event => {
-            this.updateStores(event.detail.variant)
-          })
-          this.updateStores({ id: this.defaultVariantId })
+    init() {
+      window.addEventListener('option-changed', event => {
+        this.updateStores(event.detail.variant)
+      })
+      this.updateStores({ id: this.defaultVariantId })
+    },
 
-      },
-
-      updateStores(variant: object) {
-        this.pickupLocations = window.storeAvailabilities[variant.id]
-      },
-
-      renderPopup(location: object) {
-        this.popupLocation = location
-        this.locationModal = true
+    updateStores(variant: object) {
+      const locations = window.storeAvailabilities[variant.id]
+      if (locations && typeof locations === 'object') {
+        this.pickupLocations = Object.values(locations).map(location => {
+          location.address = this.formatAddress(location.address)
+          return location
+        })
+      } else {
+        console.error(
+          'Expected an object for store availabilities, but got:',
+          locations
+        )
+        this.pickupLocations = []
       }
 
-      
-    })
-  )
+    },
+
+    formatAddress(address: string) {
+      let addressParts = address.split('<br>')
+      addressParts.pop()
+      return addressParts.join(' ')
+    },
+
+    renderPopup(location: object) {
+      this.popupLocation = location
+      this.locationModal = true
+    },
+
+  }))
 }
